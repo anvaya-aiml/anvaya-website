05293ab2a69d6e65f686401b77ce7cd99c687a98
Shreyas8905 <shreyaskulkarni75@outlook.com>
2026-01-29 20:26:31 +0530

    Security config for production

--- backend/app/main.py
+++ backend/app/main.py
@@ -29,7 +29,7 @@ app = FastAPI(
 origins = settings.cors_origins.split(",")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
